f1251cbea91aeb356a2f8c455b492b60d7a57922
juniorProgrammer <juniorProgrammer09@gmail.com>
2022-07-08 14:32:10 +0300

    added password forgot and reset routes

--- todo-app-2(redo)/Routes/userRoutes.js
+++ todo-app-2(redo)/Routes/userRoutes.js
@@ -6,11 +6,13 @@ const Router=express.Router();
 
 Router.post('/sign-up',authController.signup);
 Router.post('/login',authController.login);
+Router.post('/forgot-password',authController.forgotPassword);
+Router.patch('/forgot-password/:token',authController.resetPassword);
 
 
 Router.route('/')
       .get(authController.protect,controller.getUsers)
-      .post(controller.createUser)
+      .post(authController.protect,authController.restrictTo('admin'),controller.createUser)
 
 Router.route('/:id')
       .get(authController.protect,controller.getUser)
