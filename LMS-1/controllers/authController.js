dfd19b3e9f71f3d6235570b52b9321d12b9a5d10
juniorProgrammer <juniorProgrammer09@gmail.com>
2022-07-18 16:25:37 +0300

    added protection functionality

--- LMS-1/controllers/authController.js
+++ LMS-1/controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt =require('jsonwebtoken');
 const userModel =require('./../models/user');
-
+const {promisify}=require('util');
 /**
  * Function to create a json web token for the user
  @param {* user id} id
@@ -8,6 +8,36 @@ const userModel =require('./../models/user');
  */
 const signToken=(id)=>{
     return jwt.sign({id},process.env.JWT_SECRET,{expiresIn: process.env.JWT_SECRET_EXP});
+}
+
+exports.protect=async (req,res,next)=>{
+    try {
+        // will be using a bearer token
+        //check if authorization header is set
+        let token;
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+            token=req.headers.authorization.split(' ')[1];
+
+        }
+
+        //check if token exists
+        if(!token)return next("😥Please log in.😥");
+
+        // verify token
+        const payLoad= await promisify(jwt.verify)(token,process.env.JWT_SECRET);
+
+        console.log(payLoad);
+        // check if user with that id exists
+        const user=await userModel.findById(payLoad.id);
+
+        if(!user)return next("User cannot be found..😑😶please log in again");
+
+        req.user=user;
+        next();
+
+    } catch (err) {
+        return next(err);
+    }
 }
 
 /**
