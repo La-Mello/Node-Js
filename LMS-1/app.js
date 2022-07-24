5fd70843528ace076e96e1d19b7792915326b444
brian <juniorprogrammer09@gmail.com>
2022-07-24 10:49:36 +0300

    added other routes

--- LMS-1/app.js
+++ LMS-1/app.js
@@ -5,6 +5,7 @@ const errorHandler=require('./controllers/errorController');
 
 const studentRoutes=require('./routes/students');
 const userRoutes=require('./routes/user');
+const libraryRoutes=require('./routes/library');
 
 if(process.env.NODE_ENV === 'development')
     app.use(morgan('dev'));
@@ -15,6 +16,7 @@ app.use(express.json());
 // routes
 app.use('/api/lms/v1/users',userRoutes);
 app.use('/api/lms/v1/students',studentRoutes);
+app.use('/api/lms/v1/library',libraryRoutes);
 
 //handling errors in the global scope
 app.use(errorHandler);
