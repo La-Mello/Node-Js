7c3dcab035d2306ee48f67cc8a1eebef2fd8e526
brian <juniorprogrammer09@gmail.com>
2022-07-24 10:54:59 +0300

    added some routes and editted existing

--- LMS-1/routes/students.js
+++ LMS-1/routes/students.js
@@ -1,8 +1,20 @@
 const express=require('express');
 const studentController=require('./../controllers/student')
 const Router=express.Router();
+const libraryController=require('./../controllers/library');
+const protectRoute=require('./../utils/protect');
+
+
+//! account routes
 
 Router.post('/register',studentController.register);
 Router.post('/login',studentController.login);
+Router.post('/forgot-password',studentController.forgotPassword)
+Router.post('/reset-password',studentController.resetPassword)
+
+
+//! library routes
+Router.route('/library')
+      .get(protectRoute('student'),libraryController.getBooks)
 
 module.exports=Router;
--- LMS-1/routes/user.js
+++ LMS-1/routes/user.js
@@ -2,18 +2,21 @@ const express=require('express');
 const controller=require('./../controllers/user');
 const auth=require('./../controllers/authController');
 const Router=express.Router();
+const protectRoute=require('./../utils/protect');
 
 Router.post('/sign-up',auth.signUp);
 
 Router.post('/login',auth.login);
+Router.post('/forgot-password',auth.forgotPassword);
+Router.post('/reset-password/:token',auth.resetPassword);
 
 Router.route('/')
-      .get(auth.protect,controller.getUser)
+      .get(protectRoute('user'),controller.getUser)
       .post(controller.createUser)
 
 Router.route('/:id')
       .delete(controller.deleteUser)
-      .get(auth.protect,controller.getUserById)
-      .patch(auth.protect,controller.updateUser)
+      .get(protectRoute('user'),controller.getUserById)
+      .patch(protectRoute('user'),controller.updateUser)
 
 module.exports=Router;
