ea88456bf9795ea813f385b0437aa16f07d38ff8
brian <juniorprogrammer09@gmail.com>
2022-07-24 10:52:22 +0300

    added functionality to be able to see the error stack

--- LMS-1/controllers/errorController.js
+++ LMS-1/controllers/errorController.js
@@ -3,7 +3,8 @@ module.exports=(err,req,res,next)=>{
     // console.log(err);
     res.status(err.StatusCode || 500).json({
         status:`${err.StatusCode}`.startsWith('4')?'fail':'error',
-        message:err.message || err
+        message:err.message || err,
+        stack:err.stack
     })
     
 }
