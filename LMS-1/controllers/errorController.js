2f201c79dedf6f4d41a450bca1c38cbbed32df7d
brian <juniorprogrammer09@gmail.com>
2022-07-25 09:04:12 +0300

    a much easier way to detect both dev errors and mongoose errors

--- LMS-1/controllers/errorController.js
+++ LMS-1/controllers/errorController.js
@@ -1,10 +1,52 @@
+const errorObj=require('./../utils/error');
+
 module.exports=(err,req,res,next)=>{
 
-    // console.log(err);
-    res.status(err.StatusCode || 500).json({
-        status:`${err.StatusCode}`.startsWith('4')?'fail':'error',
-        message:err.message || err,
-        stack:err.stack
-    })
+    if(err.name === 'TokenExpiredError' || err.message.startsWith('TokenExpiredError')) err= TimeExpired(err);
     
+    if(err.name === 'ValidationError') err= ValidationError(err);
+    
+    // console.log(err.status);
+    
+
+    err.statCode=err.statCode || 500
+    err.status=`${err.statCode}`.startsWith('4')?'fail':'error'
+
+    // console.log(err.statCode);
+
+    //! handling errors in both production and development environment
+
+    if(process.env.Node_env === 'development'){
+
+        // console.log(err);
+        res.status(err.statCode).json({
+            status:err.status,
+            message:err.message || err,
+            stack:err.stack,
+            error:{
+                err
+            }
+        })
+
+    }else if(process.env.Node_env === 'production'){
+
+        res.status(err.statCode).json({
+            status:err.status,
+            message:err.message || err
+        })
+
+    }
+}
+
+//! handling specific errors out of ones created during dev
+/**
+ * function to handle time expired error
+ * @returns error object
+ */
+ const TimeExpired=()=> {
+    return new errorObj("Time expired please log in again",401);
+}
+
+const ValidationError=(err)=> {
+    return new errorObj(err.message,401);
 }
