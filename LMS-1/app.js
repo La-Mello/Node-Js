5991a99315a05d4b43823f4352e8e7be40092142
brian <juniorprogrammer09@gmail.com>
2022-07-25 08:59:27 +0300

    rendered a single page, added a handler for unhandled routes

--- LMS-1/app.js
+++ LMS-1/app.js
@@ -12,11 +12,32 @@ if(process.env.NODE_ENV === 'development')
 
 // body parser
 app.use(express.json());
+app.set('view engine','ejs');
+
+//serving html pages
+app.use(express.static('public'));
+
+//content to be used by views
+app.use('/css',express.static(__dirname+'public/css'))
+app.use('/js',express.static(__dirname+'public/js'))
+app.use('/img',express.static(__dirname+'public/img'))
+
+app.get('/',(req,res)=>{
+    res.render('index',{org:"Brian Prac"})
+})
 
 // routes
 app.use('/api/lms/v1/users',userRoutes);
 app.use('/api/lms/v1/students',studentRoutes);
 app.use('/api/lms/v1/library',libraryRoutes);
+
+//unhandled routes handling
+app.all('*',(req,res)=>{
+    
+    const msg=`Cannot ${req.method} ${req.url}...`
+
+    res.render('404',{msg});
+})
 
 //handling errors in the global scope
 app.use(errorHandler);
